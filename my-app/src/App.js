750d1551593e6704fc91eac2a6c9a4583104c36a
unknown <khush.bakht334@gmail.com>
2023-04-15 13:13:53 +0100

    new chnages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import About from './components/About';
@@ -8,7 +8,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 function App() {
@@ -48,7 +47,7 @@ function App() {
     <div className="container my-3">
     <Routes>   
           <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert}/>} />
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>} />
         </Routes>
      
 
